9fda336874f377e1f17c1ce073b925ff1786eade
PawelRogowski <pawel.rogowski.praca@gmail.com>
2022-10-15 21:55:55 +0200

    added new query selector for modal window button

--- src/buy-now-modal.js
+++ src/buy-now-modal.js
@@ -1,11 +1,13 @@
 (() => {
   const refs = {
     openModalBtn: document.querySelector('[data-buy-now-modal-open]'),
+    openModalBtnMobile: document.querySelector('[data-buy-now-modal-open-mobile]'),
     closeModalBtn: document.querySelector('[data-buy-now-modal-close]'),
     modal: document.querySelector('[data-buy-now-modal]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.openModalBtnMobile.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
