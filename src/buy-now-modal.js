8959d00bb5bb4ac2eab347e6f5fa70c57473d882
Pawel Gorniak <p_gorniak@tlen.pl>
2022-10-15 19:44:52 +0200

    Create buy-now-modal window

--- src/buy-now-modal.js
+++ src/buy-now-modal.js
@@ -1,8 +1,8 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
+    openModalBtn: document.querySelector('[data-buy-now-modal-open]'),
+    closeModalBtn: document.querySelector('[data-buy-now-modal-close]'),
+    modal: document.querySelector('[data-buy-now-modal]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
